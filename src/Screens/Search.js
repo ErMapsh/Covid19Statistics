31847ef610f6c3d1a11268f5f6ca17e461432166
ErMapsh <maheshmestri73@gmail.com>
2022-03-30 14:17:36 +0530

    search screen done

--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -1,85 +1,168 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, SafeAreaView, FlatList, TextInput} from 'react-native';
-import {style} from '../Stylesheet/SearchStyle';
+import React, { useState, useContext, useEffect } from 'react';
+import {
+  Text,
+  FlatList,
+  TextInput,
+  StyleSheet,
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  Modal
+} from 'react-native';
 import axios from 'axios';
+import { Card } from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { black } from 'react-native-paper/lib/typescript/styles/colors';
 
 export default function Search() {
-  const [data, setdata] = useState([]);
-  const [MasterData, setMasterData] = useState([]);
-  const [Search, setSearch] = useState('');
-  const fetchapi = async () => {
+  const [DateByApi, setDateByApi] = useState('')
+  const [search, setsearch] = useState('');
+  const [StateData, setStateData] = useState([])//this is main master data
+  const [FilterData, setFilterData] = useState([])
+  // console.log(search)
+
+  const fetchStateData = async () => {
     try {
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      // console.log(res.data);
-      setdata(res.data);
-      setMasterData(res.data);
-    } catch (error) {
-      console.error(error);
+      const res = await axios.get('https://ermapsh.github.io/MyAPis/stateData.json')
+      // console.log(res.data)
+      setStateData(res.data.states);
+      setFilterData(res.data.states)
+      setDateByApi(res.data.date);
+    } catch {
+      setStateData({ "error": "Newtork Error" })
     }
-  };
+  }
 
   useEffect(() => {
-    fetchapi();
-  }, []);
+    fetchStateData();
+  }, [])
 
-  const ItemView = ({item}) => {
-    return (
-      <Text style={{padding: 10, fontWeight: 'bold'}}>
-        {item.id}
-        {'. '}
-        {item.title.toUpperCase()}
-      </Text>
-    );
-  };
 
-  const ItemSeparatorComponent = () => {
-    return (
-      <View style={{height: 0.5, width: '100%', backgroundColor: '#c8c8c8'}} />
-    );
-  };
-  const searchFilter = text => {
+  //for modal
+  const [modalVisible, setmodalVisible] = useState(false)
+  const [ForModalData, setForModalData] = useState('')
+  const OpenModal = (item) => {
+    setmodalVisible(true)
+    setForModalData(item);
+  }
+
+
+  // for searching
+  const SearchFilterFuntion = (text) => {
     if (text) {
-      const NewData = data.filter(item => {
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : ''.toUpperCase();
+      const NewData = StateData.filter((item) => {
+        const itemData = item.state ? item.state.toUpperCase() : ''.toUpperCase();
         const textData = text.toUpperCase();
         return itemData.indexOf(textData) > -1;
-      });
-      setdata(NewData);
-      setSearch(text)
-    }else{
-      setdata(MasterData);
-      setSearch(text) 
+      })
+      setFilterData(NewData);
+      setsearch(text);
+    } else {
+      setFilterData(StateData);
+      setsearch(text)
     }
-  };
+  }
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{backgroundColor: 'white'}}>
-        <TextInput
-          value={Search}
-          placeholder="Search Here"
-          underlineColorAndroid="transparent"
-          onChangeText={text => {
-            searchFilter(text);
-          }}
-          style={{
-            height: 40,
-            borderWidth: 1,
-            paddingLeft: 20,
-            margin: 20,
-            borderColor: '#009688',
-            backgroundColor: 'white',
-            borderRadius: 25,
-          }}
-        />
+    <View style={style.main} >
+
+      {/* Text Input */}
+      <Modal visible={modalVisible} animationType='slide'>
+        <View style={style.ModalBackground}>
+
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, }}>
+            {/* for close  */}
+            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View></View>
+              <View>
+                <TouchableOpacity onPress={() => { setmodalVisible(false) }}>
+                  <Ionicons name="close" size={25} color={'black'} />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <Text style={style.font}>Date: {DateByApi}</Text>
+            <Text style={style.font}>State Name: {ForModalData.state}</Text>
+            <Text style={style.font}>Total Confirmed: {ForModalData.Total_Confirmed}</Text>
+            <Text style={style.font}>Total Discharged: {ForModalData.Discharged}</Text>
+            <Text style={style.font}>Total Deaths: {ForModalData.Deaths}</Text>
+          </View>
+        </View>
+      </Modal>
+
+      <TextInput
+        value={search}
+        placeholder="Search Here"
+        underlineColorAndroid="transparent"
+        onChangeText={text => {
+          SearchFilterFuntion(text);
+        }}
+        style={style.textInput}
+      />
+
+
+      <View style={{ height: '90%' }}>
         <FlatList
-          data={data}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={ItemSeparatorComponent}
-          renderItem={ItemView}
+          data={FilterData}
+          renderItem={({ item }) => (
+
+            <View style={style.ctn}>
+              <TouchableOpacity onPress={() => { OpenModal(item) }}>
+                <Card style={style.cardContainer}>
+                  <Text style={[style.card, style.fontForstate]}>{item.state}</Text>
+                </Card>
+              </TouchableOpacity>
+            </View>
+          )}
         />
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
+
+const style = StyleSheet.create({
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    backgroundColor: "#4FD3C4"
+  },
+  textInput: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 20,
+    borderColor: '#009688',
+    backgroundColor: 'white',
+    borderRadius: 25,
+
+  },
+  card: {
+    padding: 8,
+    textAlign: 'center',
+    shadowColor: 'black',
+  },
+  cardContainer: {
+    margin: 8,
+    width: Dimensions.get('window').width - 100,
+  },
+  ctn: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fontForstate: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+
+
+  //for modal
+  ModalBackground: { flex: 1, 'backgroundColor': 'rgba(0,0,0,0.5)', justifyContent: 'center', alignItems: 'center' },
+  font: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: "black",
+  }
+});
+
--- src/Stylesheet/SearchStyle.js
+++ src/Stylesheet/SearchStyle.js
@@ -2,15 +2,9 @@ import {Dimensions, StyleSheet} from 'react-native';
 
 
 const style = StyleSheet.create({
-    container:{
-        backgroundColor: 'white',
-    },
-    text:{
-        fontWeight: 'bold',
-        fontSize: 45,
-    },
-    itemStyle:{
-        padding: 10,
+    textInput:{
+        borderWidth: 2,
+        borderColor: "black",
     }
 
 })
